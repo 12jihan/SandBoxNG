3fd5cbb91c9d94f65a908fd9513a055cb0be505d
Jareem Hoff <jareem.e.hoff@gmail.com>
2024-05-03 17:06:49 -0400

    made more changes

---
+++ src/app/Game.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+class Game {
+    // Add member functions and variables here
+public:
+    void start() {
+        // Code for starting the game
+    }
+
+    void update() {
+        // Code for updating the game state
+    }
+
+    void render() {
+        // Code for rendering the game
+    }
+};
---
+++ src/app/WindowManager.cpp
@@ -0,0 +1,10 @@
+// Include necessary headers
+
+class WindowManager {
+};
+
+// Implement the constructor and destructor
+
+// Implement the public member functions
+
+// Implement the private member functions
