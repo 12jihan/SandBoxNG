121f11e779a7d52d126376047c00b1768ac7a880
Jareem Hoff <jareem.e.hoff@gmail.com>
2024-07-01 03:28:08 -0400

    created the window surface

--- src/app/Vk_Window_Surface.cpp
+++ src/app/Vk_Window_Surface.cpp
@@ -1,16 +1,34 @@
 #include "./includes/Vk_Window_Surface.hpp"
 
-#ifdef _WIN32
-#define VK_USE_PLATFORM_WIN32_KHR
-#define GLFW_EXPOSE_NATIVE_WIN32
-#elif __APPLE__
-#define VK_USE_PLATFORM_MACOS_MVK
-#define GLFW_EXPOSE_NATIVE_COCOA
-#elif __LINUX__
-#define VK_USE_PLATFORM_XCB_KHR
-#define GLFW_EXPOSE_NATIVE_X11
-#endif
+#include <iostream>
 
+#ifdef __APPLE__
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-#include <GLFW/glfw3native.h>
+#include <vulkan/vulkan.h>
+#include <vulkan/vulkan_beta.h>
+#endif
+
+void Vk_Window_Surface::init(VkInstance &instance, GLFWwindow *window) {
+    _instance = instance;
+    _window = window;
+    _create_surface();
+}
+
+void Vk_Window_Surface::_create_surface() {
+    VkSurfaceKHR surface;
+    VkResult result = glfwCreateWindowSurface(_instance, _window, nullptr, &_surface);
+    if (result != VK_SUCCESS) {
+        throw std::runtime_error("failed to create window surface!");
+    }
+}
+
+VkSurfaceKHR Vk_Window_Surface::get_surface() {
+    return _surface;
+}
+
+void Vk_Window_Surface::clean() {
+    if (_surface != VK_NULL_HANDLE) {
+        vkDestroySurfaceKHR(_instance, _surface, nullptr);
+    }
+}
--- src/app/includes/Vk_Window_Surface.hpp
+++ src/app/includes/Vk_Window_Surface.hpp
@@ -1,9 +1,36 @@
 #pragma once
+
+// #ifdef _WIN32
+// #define VK_USE_PLATFORM_WIN32_KHR
+// #define GLFW_EXPOSE_NATIVE_WIN32
+// #include <GLFW/glfw3native.h>
+// #elif __APPLE__
+// #define VK_USE_PLATFORM_METAL_EXT
+// #define GLFW_EXPOSE_NATIVE_COCOA
+// #include <GLFW/glfw3native.h>
+// #include <vulkan/vulkan_metal.h>
+// #elif __LINUX__
+// #define VK_USE_PLATFORM_XCB_KHR
+// #define GLFW_EXPOSE_NATIVE_X11
+// #include <GLFW/glfw3native.h>
+// #endif
+
+#ifdef __APPLE__
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 #include <vulkan/vulkan.h>
+#include <vulkan/vulkan_beta.h>
+#endif
 
 class Vk_Window_Surface {
    private:
-    VkSurfaceKHR _surface;
+    VkSurfaceKHR _surface = VK_NULL_HANDLE;
+    VkInstance _instance;
+    GLFWwindow* _window;
+    void _create_surface();
 
    public:
+    void init(VkInstance& instance, GLFWwindow* window);
+    void clean();
+    VkSurfaceKHR get_surface();
 };
