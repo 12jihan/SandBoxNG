e279dfff4b59ac73324b93da2879146328c43fd0
Jareem Hoff <jareem.e.hoff@gmail.com>
2024-12-23 02:15:55 -0500

    update findif function

--- src/app/tools/Blk_Tools.cpp
+++ src/app/tools/Blk_Tools.cpp
@@ -3,7 +3,12 @@
 #include <vector>
 
 bool Blk_Tools::vector_contains(const std::vector<const char*>& vec, const char* element) {
-    return std::find_if(vec.begin(), vec.end(), [element](const char* str) {
-               return std::strcmp(str, element) == 0;
-           }) != vec.end();
+    bool found = std::find_if(
+        vec.begin(), 
+        vec.end(), 
+        [element](const char* str) {
+            return std::strcmp(str, element) == 0;
+        }
+    ) != vec.end();
+    return found;
 };
